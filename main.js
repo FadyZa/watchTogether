ad1ae97edd5cf44ec00785ac41997401fba9aae0
FadyZa <92400077+FadyZa@users.noreply.github.com>
2023-02-14 17:07:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const video = document.querySelector("#myVideo");
 const currentTime = document.querySelector(".current")
 const totalTime = document.querySelector(".total")
+const urlBtn = document.getElementById("urlBtn");
 const playPauseBtn = document.querySelector(".play-pause");
 const videoContainer = document.querySelector("section");
 const miniBtn = document.querySelector(".mini-btn");
@@ -9,9 +10,128 @@ const volumeBtn = document.querySelector(".volume-btn");
 const volumeSlider = document.getElementById("vol-slider");
 const progressBar = document.getElementById("progress-bar");
 const seek = document.querySelector(".seek")
+//////////////////////////////////////////////////
+
+/////////////////////////////////////////// Real Time DATABASE ///////////////////////////////////////////
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfD7sEdS4d25lfq3TOzb9Vw8wXiNGEDlA",
+  authDomain: "watchtogether-d4d1a.firebaseapp.com",
+  databaseURL: "https://watchtogether-d4d1a-default-rtdb.firebaseio.com",
+  projectId: "watchtogether-d4d1a",
+  storageBucket: "watchtogether-d4d1a.appspot.com",
+  messagingSenderId: "273247284108",
+  appId: "1:273247284108:web:7eb3867bdb372c8727c4a9",
+  measurementId: "G-2T83JS8LHG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+import { getDatabase, set, ref, onValue, update,remove } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+const database = getDatabase();
+// Get the real-time database reference
+const myVideo = ref(database, 'videos');
+
+// Update the current time in the database when it changes
+// initialize video Data 
+function initialize(src,dur){
+    set(myVideo, {
+        vidSrc:src,
+        total:dur,
+        time: 0,
+        playing:false
+    }).then(()=>{
+        console.log("writed")
+    },()=>{
+        console.log("failed")
+    });
+}
+
+function updateStatus(playing){
+    update(myVideo,{
+        time:video.currentTime,
+        playing: playing
+    });
+}
+
+video.addEventListener("play",()=>{
+    videoContainer.classList.remove("paused")
+    updateStatus(true)
+})
+
+video.addEventListener("pause",()=>{
+    updateStatus(false)
+})
+
+seek.addEventListener("input",(e)=>{
+    skipAhead(e)
+    update(myVideo,{
+        time:video.currentTime
+    });
+})
+
+// Update the video player when the current time changes in the database
+onValue(myVideo, (snapshot) => {
+    if (snapshot.exists()){
+        const videoData = snapshot.val();
+        const vidSrc = videoData.vidSrc;
+        videoContainer.classList.remove("hide");
+        
+        if (video.src !== vidSrc) {
+            video.src = vidSrc;
+        }
+        
+        const totalTime = videoData.total;
+        const currentTime = videoData.time;
+        const playing = videoData.playing;
+        
+        setTotalTime(totalTime); 
+    
+        if (playing) {
+            video.play();
+            video.currentTime = currentTime;
+        } else {
+            video.pause();
+            console.log(playing)
+            videoContainer.classList.add("paused");
+        }
+    
+        // Only update the video player if the time is different
+        if (Math.abs(video.currentTime - currentTime) > 0.1) {
+            video.currentTime = currentTime;
+        }
+    }
+});
+
+///////////////////////////////////////////////////////////////////
+
+// add video src 
+urlBtn.addEventListener("click", () => {
+    const vidSrc = document.getElementById("vidSrc").value;
+    video.src = vidSrc;
+
+    video.addEventListener("loadeddata", () => {
+        console.log("Video loaded");
+
+        remove(myVideo).then(() => {
+            console.log("Removed old video");
+            initialize(vidSrc, video.duration);
+        }).catch((error) => {
+            console.error("Failed to remove old video", error);
+        });
+    },{ once: true });
+});
 
 
-// timeline and duration control /////////////////////
+
+// timeline and duration CONTROL /////////////////////
 function formatTime(duration){
     const result = new Date(duration * 1000).toISOString().substr(11, 8);
     return {
@@ -20,25 +140,24 @@ function formatTime(duration){
     };
 };
 
-video.addEventListener("loadeddata",()=>{
-    const total = formatTime(video.duration);
-    console.log(total)
+function setTotalTime(myDuration){
+    const total = formatTime(myDuration);
     totalTime.innerHTML = `${total.minutes}:${total.seconds}`;
     const videoDuration = Math.round(video.duration);
     progressBar.setAttribute('max',videoDuration)
     seek.setAttribute('max',videoDuration)
-})
+};
 
 video.addEventListener("timeupdate",()=>{
     let curent = formatTime(video.currentTime);
     currentTime.textContent =`${curent.minutes}:${curent.seconds}`;
+    updateProgress();
 })
 
 function updateProgress() {
     seek.value = Math.floor(video.currentTime);
     progressBar.value = Math.floor(video.currentTime);
 }
-video.addEventListener('timeupdate', updateProgress);
 
 // the input range is moved 
 function skipAhead(event) {
@@ -47,7 +166,6 @@ function skipAhead(event) {
     progressBar.value = skipTo;
     seek.value = skipTo;
 }
-seek.addEventListener('input', skipAhead);
 
 // play and pause buttons CONTROL /////////////////////
 function togglePlay(){
@@ -57,11 +175,6 @@ function togglePlay(){
 
 playPauseBtn.addEventListener("click",togglePlay);
 video.addEventListener("click",togglePlay);
-
-video.addEventListener("play",()=>{
-    videoContainer.classList.remove("paused")
-})
-
 
 // full screen button CONTROL /////////////////////
 fullBtn.addEventListener("click",()=>{
@@ -113,92 +226,3 @@ video.addEventListener("volumechange",()=>{
     }
     videoContainer.dataset.volume = volumeLevel;
 })
-
-/////////////////////////////////////////// Real Time DATABASE ///////////////////////////////////////////
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBfD7sEdS4d25lfq3TOzb9Vw8wXiNGEDlA",
-  authDomain: "watchtogether-d4d1a.firebaseapp.com",
-  databaseURL: "https://watchtogether-d4d1a-default-rtdb.firebaseio.com",
-  projectId: "watchtogether-d4d1a",
-  storageBucket: "watchtogether-d4d1a.appspot.com",
-  messagingSenderId: "273247284108",
-  appId: "1:273247284108:web:7eb3867bdb372c8727c4a9",
-  measurementId: "G-2T83JS8LHG"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Realtime Database and get a reference to the service
-import { getDatabase, set, ref, onValue, update } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-const database = getDatabase();
-
-// Get the real-time database reference
-const myVideo = ref(database, 'videos');
-
-// Update the current time in the database when it changes
-video.addEventListener("loadeddata",()=>{
-    set(myVideo, {
-        time: video.currentTime,
-        playing:false
-    }).then(()=>{
-        console.log("writed")
-    },()=>{
-        console.log("failed")
-    });
-})
-
-video.addEventListener("play",()=>{
-    update(myVideo,{
-        time:video.currentTime,
-        playing: true
-    });
-})
-
-video.addEventListener("pause",()=>{
-    update(myVideo,{
-        time:video.currentTime,
-        playing: false
-    });
-})
-
-seek.addEventListener("input",()=>{
-    update(myVideo,{
-        time:video.currentTime
-    });
-})
-
-
-// Update the video player when the current time changes in the database
-onValue(myVideo, (snapshot) => {
-    const videoData = snapshot.toJSON();
-    const currentTime = videoData.time;
-    const playing = videoData.playing;
-    
-    if (playing) {
-        console.log(currentTime)
-        video.play();
-        video.currentTime = currentTime;
-    }
-    else {
-        video.pause();
-        videoContainer.classList.add("paused")
-    }
-
-    // Only update the video player if the time is different
-    if (Math.abs(video.currentTime - currentTime) > 0.1) {
-        video.currentTime = currentTime;
-    }
-})
-
-
-
-
-
